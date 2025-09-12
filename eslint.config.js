721eedb986eeba14467cd897c100e3702d64e60a
Raunak Bhagat <r@rabh.io>
2025-09-12 16:47:01 -0700

    Add basic infra

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import js from "@eslint/js";
+import typescript from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    languageOptions: {
+      parser: typescriptParser,
+      ecmaVersion: 2020,
+      sourceType: "module",
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        Buffer: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        global: "readonly",
+        module: "readonly",
+        require: "readonly",
+        exports: "readonly",
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescript,
+      prettier: prettier,
+    },
+    rules: {
+      ...prettierConfig.rules,
+      "prettier/prettier": "warn",
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
+];
