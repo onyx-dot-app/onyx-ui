ddcb01a78dc9be0844c0821a213db7250b7a49db
Raunak Bhagat <r@rabh.io>
2025-09-15 17:20:27 -0700

    Ignore `dist` directory

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,9 @@ import prettierConfig from "eslint-config-prettier";
 export default [
   js.configs.recommended,
   {
+    ignores: ["dist/**", "node_modules/**", "*.config.js", "*.config.ts"],
+  },
+  {
     files: ["**/*.{ts,tsx,js,jsx}"],
     languageOptions: {
       parser: typescriptParser,
